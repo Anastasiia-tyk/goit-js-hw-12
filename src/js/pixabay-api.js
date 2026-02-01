43e53807a3ce96c51d9ad71f49e4ed72a87175da
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-02-01 10:25:39 +0200

    update js (pixabay-api)

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,15 +3,22 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '54459012-5e63b86c877094f2e2839be1c'; 
 
-export function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1) {
     const params = {
         key: API_KEY,
         q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: 12,
+        page: page,
     };
 
-    return axios.get(BASE_URL, { params })
-        .then(response => response.data);
+    try {
+        const response = await axios.get(BASE_URL, { params });
+        return response.data;
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
